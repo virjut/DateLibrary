e318adee6b3cd689a8478b5e222185fc9c46de3f
juttavirta <jutta563@gmail.com>
2022-06-27 13:47:58 +0300

    aikafunktio luominen

--- DateLibrary/Class1.cs
+++ DateLibrary/Class1.cs
@@ -2,7 +2,23 @@
 
 namespace DateLibrary
 {
-    public class Class1
+    public class Aikafunktio
     {
+        public static bool Onkotulevaisuudessa(DateTime paivamaara) 
+        {
+            int result = DateTime.Compare(paivamaara, DateTime.Now);
+
+            Console.WriteLine(result);
+
+            if (result < 0)
+                return false;
+            else if (result == 0)
+                return false;
+            else
+                return true;
+
+        }
+
+       
     }
 }
